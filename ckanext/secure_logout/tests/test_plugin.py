b0c9a4da3a1fd499602b702151d1a7840e7ed05c
Fisnik Bedrija <fisnik.bedrija@gmail.com>
2025-12-17 09:39:33 +0100

    fix CI error by adding imports

--- ckanext/secure_logout/tests/test_plugin.py
+++ ckanext/secure_logout/tests/test_plugin.py
@@ -47,6 +47,8 @@ To temporary patch the CKAN configuration for the duration of a test you can use
     def test_some_action():
         pass
 """
+import pytest
+from ckan.plugins import plugin_loaded
 import ckanext.secure_logout.plugin as plugin
 
 
